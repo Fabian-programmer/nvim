0eba535ee04798704216fd3dc90dcb4194c84674
Fabian-programmer <haefab@gmail.com>
2023-02-03 22:25:25 +0100

    Add leap plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -30,6 +30,20 @@ return {
     end,
   },
 
+  -- easily jump to any location and enhanced f/t motions for Leap
+  {
+    "ggandor/leap.nvim",
+    event = "VeryLazy",
+    dependencies = { { "ggandor/flit.nvim", opts = { labeled_modes = "nv" } } },
+    config = function(_, opts)
+      local leap = require("leap")
+      for k, v in pairs(opts) do
+        leap.opts[k] = v
+      end
+      leap.add_default_mappings(true)
+    end,
+  },
+
 
   {
     "numToStr/Comment.nvim",
