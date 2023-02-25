90aaa393fc7010b2d1629a5f8d54fe580e1b2102
Fabian-programmer <haefab@gmail.com>
2023-02-25 19:26:23 +0100

    Speed up intend animation

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -133,7 +133,6 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     event = { "BufReadPost", "BufNewFile" },
     opts = {
-      -- char = "▏",
       char = "│",
       filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
       show_trailing_blankline_indent = false,
@@ -144,16 +143,15 @@ return {
   -- active indent guide and indent text objects
   {
     "echasnovski/mini.indentscope",
-    version = false, -- wait till new 0.7.0 release to put it back on semver
     event = { "BufReadPre", "BufNewFile" },
     opts = {
-      -- symbol = "▏",
+      draw = { delay = 10 },
       symbol = "│",
       options = { try_as_border = true },
     },
     config = function(_, opts)
       vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+        pattern = { "help", "alpha", "dashboard", "NvimTree", "Trouble", "lazy", "mason" },
         callback = function()
           vim.b.miniindentscope_disable = true
         end,
