9cd69bcd9277e5ec1eeb1cc8e6820b3c06a6c1ed
Fabian Haeussler <Fabian.Haeussler@de.bosch.com>
2023-03-18 15:53:59 +0100

    Use live grep with arguments

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -48,13 +48,14 @@ return {
   "nvim-telescope/telescope.nvim",
   cmd = { "Telescope" },
   keys = {
-    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File" },
-    { "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Open Recent File" },
-    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
+    { "<leader>ff", "<cmd>Telescope find_files<cr>",                desc = "Find File" },
+    { "<leader>fo", "<cmd>Telescope oldfiles<cr>",                  desc = "Open Recent File" },
+    { "<leader>fg", "<cmd>Telescope live_grep_args<cr>",            desc = "Grep" },
     { "<leader>fb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
-    { "<leader>fh", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-    { "<leader>fp", "<cmd>Telescope project<cr>", desc = "Project" },
-    { "<leader>fs",
+    { "<leader>fh", "<cmd>Telescope command_history<cr>",           desc = "Command History" },
+    { "<leader>fp", "<cmd>Telescope project<cr>",                   desc = "Project" },
+    {
+      "<leader>fs",
       function()
         require("telescope.builtin").lsp_document_symbols({
           symbols = {
@@ -70,25 +71,25 @@ return {
             "Property",
           },
         })
-      end, desc = "Goto Symbol" },
-    { "<leader>fm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
-
-    { "<leader>ht", "<cmd>Telescope builtin<cr>", desc = "Telescope" },
-    { "<leader>hc", "<cmd>Telescope commands<cr>", desc = "Commands" },
-    { "<leader>hh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
-    { "<leader>hm", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
-    { "<leader>hk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
-    { "<leader>hs", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
-
-    { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Commits" },
+      end,
+      desc = "Goto Symbol"
+    },
+    { "<leader>fm", "<cmd>Telescope marks<cr>",        desc = "Jump to Mark" },
+    { "<leader>ht", "<cmd>Telescope builtin<cr>",      desc = "Telescope" },
+    { "<leader>hc", "<cmd>Telescope commands<cr>",     desc = "Commands" },
+    { "<leader>hh", "<cmd>Telescope help_tags<cr>",    desc = "Help Pages" },
+    { "<leader>hm", "<cmd>Telescope man_pages<cr>",    desc = "Man Pages" },
+    { "<leader>hk", "<cmd>Telescope keymaps<cr>",      desc = "Key Maps" },
+    { "<leader>hs", "<cmd>Telescope highlights<cr>",   desc = "Search Highlight Groups" },
+    { "<leader>gc", "<cmd>Telescope git_commits<cr>",  desc = "Commits" },
     { "<leader>gb", "<cmd>Telescope git_branches<cr>", desc = "Branches" },
-    { "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "Status" },
+    { "<leader>gs", "<cmd>Telescope git_status<cr>",   desc = "Status" },
   },
-
   dependencies = {
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    { "nvim-telescope/telescope-fzf-native.nvim",    build = "make" },
     { "nvim-telescope/telescope-project.nvim" },
     { "nvim-telescope/telescope-dap.nvim" },
+    { "nvim-telescope/telescope-live-grep-args.nvim" },
   },
   config = function()
     local telescope = require("telescope")
@@ -149,9 +150,22 @@ return {
           }
         },
       },
+      extensions = {
+        live_grep_args = {
+          auto_quoting = true,
+          mappings = {
+            i = {
+              ["<C-k>"] = require("telescope-live-grep-args.actions").quote_prompt(),
+              ["<C-i>"] = require("telescope-live-grep-args.actions").quote_prompt({ postfix = " --iglob " }),
+              ["<C-a>"] = require("telescope-live-grep-args.actions").quote_prompt({ postfix = " --no-ignore " }),
+            },
+          },
+        }
+      }
     })
     telescope.load_extension("fzf")
     telescope.load_extension("project")
     telescope.load_extension("dap")
+    telescope.load_extension("live_grep_args")
   end,
 }
