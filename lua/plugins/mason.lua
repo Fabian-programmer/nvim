9b8b99f3eaa1ed4a0ec9df5e21b15667b4fb5e90
Fabian-programmer <haefab@gmail.com>
2023-03-26 19:04:55 +0200

    Clean up mason.lua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,34 +4,34 @@ local M = {
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
   },
-}
-
-M.tools = {
-  "clangd",
-  "pyright",
-  "clang-format",
-  "black",
-  "cmake-language-server",
-  "prettier",
-  "lua-language-server",
-}
-
-function M.check()
-  local mr = require("mason-registry")
-  for _, tool in ipairs(M.tools) do
-    local p = mr.get_package(tool)
-    if not p:is_installed() then
-      p:install()
+  opts = {
+    ensure_installed = {
+      "clangd",
+      "pyright",
+      "clang-format",
+      "black",
+      "cmake-language-server",
+      "prettier",
+      "lua-language-server",
+    },
+  },
+  config = function(_, opts)
+    require("mason").setup(opts)
+    local mr = require("mason-registry")
+    local function ensure_installed()
+      for _, tool in ipairs(opts.ensure_installed) do
+        local p = mr.get_package(tool)
+        if not p:is_installed() then
+          p:install()
+        end
+      end
     end
-  end
-end
-
-function M.config()
-  require("mason").setup()
-  M.check()
-  require("mason-lspconfig").setup({
-    automatic_installation = true,
-  })
-end
+    if mr.refresh then
+      mr.refresh(ensure_installed)
+    else
+      ensure_installed()
+    end
+  end,
+}
 
 return M
