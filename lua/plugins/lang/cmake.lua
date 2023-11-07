0d0994e02226341a089d3fc66c4c2d8170867b82
Fabian Haeussler <Fabian.Haeussler@de.bosch.com>
2023-11-07 10:55:50 +0100

    add cmake linter

--- lua/plugins/lang/cmake.lua
+++ lua/plugins/lang/cmake.lua
@@ -31,7 +31,7 @@ return {
     "mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "cmakelang" })
+      vim.list_extend(opts.ensure_installed, { "cmakelang", "cmakelint" })
     end,
   },
   {
@@ -45,6 +45,6 @@ return {
   {
     "Civitasv/cmake-tools.nvim",
     opts = {},
-    event = {"BufReadPre", "BufNewFile"},
+    event = { "BufReadPre", "BufNewFile" },
   },
 }
