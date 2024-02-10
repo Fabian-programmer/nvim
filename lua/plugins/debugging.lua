5cfb1f8b5d455fe5b721016141ca095779a27acd
Fabian-programmer <haefab@gmail.com>
2024-02-10 11:43:30 +0100

    rem dap virt text

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -40,12 +40,6 @@ return {
         end,
       },
 
-      -- virtual text for the debugger
-      {
-        "theHamsta/nvim-dap-virtual-text",
-        opts = {},
-      },
-
       -- which key integration
       {
         "folke/which-key.nvim",
