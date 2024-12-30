5e5fa2ed3d8bec5ca659199b751bac792857ce8c
Fabian-programmer <haefab@gmail.com>
2024-12-30 19:39:41 +0100

    simplify blink setup

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -5,24 +5,11 @@ return {
     "saghen/blink.cmp",
     version = "*",
     build = "cargo build --release",
-    opts_extend = {
-      "sources.completion.enabled_providers",
-      "sources.compat",
-      "sources.default",
-    },
-    dependencies = {
-      "rafamadriz/friendly-snippets",
-      -- add blink.compat to dependencies
-      {
-        "saghen/blink.compat",
-        optional = true, -- make optional so it's only enabled if any extras need it
-        opts = {},
-        version = "*",
-      },
-    },
+    dependencies = "rafamadriz/friendly-snippets",
     event = "InsertEnter",
 
     opts = {
+      -- disables autocomplete in rename ui input
       enabled = function()
         return not vim.tbl_contains({}, vim.bo.filetype)
           and vim.bo.buftype ~= "nofile"
@@ -49,15 +36,11 @@ return {
           auto_show_delay_ms = 200,
         },
         ghost_text = {
-          enabled = vim.g.ai_cmp,
+          enabled = true,
         },
       },
 
-      -- experimental signature help support
-      -- signature = { enabled = true },
-
       sources = {
-        compat = {},
         default = { "lsp", "path", "snippets", "buffer" },
         cmdline = {},
       },
@@ -67,57 +50,5 @@ return {
         ["<C-y>"] = { "select_and_accept" },
       },
     },
-    config = function(_, opts)
-      local enabled = opts.sources.default
-      for _, source in ipairs(opts.sources.compat or {}) do
-        opts.sources.providers[source] = vim.tbl_deep_extend(
-          "force",
-          { name = source, module = "blink.compat.source" },
-          opts.sources.providers[source] or {}
-        )
-        if type(enabled) == "table" and not vim.tbl_contains(enabled, source) then
-          table.insert(enabled, source)
-        end
-      end
-
-      -- Unset custom prop to pass blink.cmp validation
-      opts.sources.compat = nil
-
-      -- check if we need to override symbol kinds
-      for _, provider in pairs(opts.sources.providers or {}) do
-        if provider.kind then
-          local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-          local kind_idx = #CompletionItemKind + 1
-
-          CompletionItemKind[kind_idx] = provider.kind
-          CompletionItemKind[provider.kind] = kind_idx
-
-          local transform_items = provider.transform_items
-          provider.transform_items = function(ctx, items)
-            items = transform_items and transform_items(ctx, items) or items
-            for _, item in ipairs(items) do
-              item.kind = kind_idx or item.kind
-            end
-            return items
-          end
-
-          -- Unset custom prop to pass blink.cmp validation
-          provider.kind = nil
-        end
-      end
-
-      require("blink.cmp").setup(opts)
-    end,
-  },
-
-  -- add icons
-  {
-    "saghen/blink.cmp",
-    opts = function(_, opts)
-      opts.appearance = opts.appearance or {}
-      opts.appearance.kind_icons = vim.tbl_extend("keep", {
-        Color = "██", -- Use block instead of icon for color items to make swatches more usable
-      }, require("config").icons.kinds)
-    end,
   },
 }
