975fbb5c0787cd02b675279d040d877d41a4b0b3
Fabian-programmer <haefab@gmail.com>
2024-12-30 18:46:56 +0100

    disable auto-completion in rename

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -23,6 +23,12 @@ return {
     event = "InsertEnter",
 
     opts = {
+      enabled = function()
+        return not vim.tbl_contains({}, vim.bo.filetype)
+          and vim.bo.buftype ~= "nofile"
+          and vim.bo.buftype ~= "prompt"
+          and vim.b.completion ~= false
+      end,
       appearance = {
         use_nvim_cmp_as_default = false,
         nerd_font_variant = "mono",
