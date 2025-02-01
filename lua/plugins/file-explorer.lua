7be25bce1c5d4af8d2b7199bfc1c3d3c244b32a9
Fabian-programmer <haefab@gmail.com>
2025-02-01 14:58:54 +0100

    add scratch buffer

--- lua/plugins/file-explorer.lua
+++ lua/plugins/file-explorer.lua
@@ -6,21 +6,19 @@ return {
         replace_netrw = true,
       },
     },
+    -- stylua: ignore
     keys = {
-      {
-        "<leader>e",
-        function()
-          Snacks.explorer({ cwd = require("util").get_root() })
-        end,
-        desc = "File Explorer (root dir)",
-      },
-      {
-        "<leader>E",
-        function()
-          Snacks.explorer()
-        end,
-        desc = "File Explorer (cwd)",
-      },
+      { "<leader>e", function() Snacks.explorer({ cwd = require("util").get_root() }) end, desc = "File Explorer (root dir)" },
+      { "<leader>E", function() Snacks.explorer() end, desc = "File Explorer (cwd)" },
+    },
+  },
+
+  {
+    "folke/snacks.nvim",
+    -- stylua: ignore
+    keys = {
+      { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+      { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
     },
   },
 }
