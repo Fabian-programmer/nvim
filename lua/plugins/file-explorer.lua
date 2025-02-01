bf096b4a547a2e2e428031936c9c3d888e6f0f1e
Fabian-programmer <haefab@gmail.com>
2025-02-01 14:15:31 +0100

    use snacks fileexplorer

--- lua/plugins/file-explorer.lua
+++ lua/plugins/file-explorer.lua
@@ -1,78 +1,26 @@
 return {
   {
-    "nvim-neo-tree/neo-tree.nvim",
-    cmd = "Neotree",
-    dependencies = {
-      { "MunifTanjim/nui.nvim", lazy = true },
+    "folke/snacks.nvim",
+    opts = {
+      explorer = {
+        replace_netrw = true,
+      },
     },
     keys = {
       {
         "<leader>e",
         function()
-          require("neo-tree.command").execute({ toggle = true, dir = require("util").get_root() })
+          Snacks.explorer({ cwd = require("util").get_root() })
         end,
         desc = "File Explorer (root dir)",
       },
       {
         "<leader>E",
         function()
-          require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+          Snacks.explorer()
         end,
         desc = "File Explorer (cwd)",
       },
     },
-    opts = {
-      open_files_do_not_replace_types = { "trouble", "qf" },
-      -- If you don't want to use these columns, you can set `enabled = false` for each of them individually
-      file_size = {
-        enabled = false,
-      },
-      type = {
-        enabled = false,
-      },
-      last_modified = {
-        enabled = false,
-      },
-      created = {
-        enabled = false,
-      },
-      symlink_target = {
-        enabled = true,
-      },
-      filesystem = {
-        bind_to_cwd = true,
-        follow_current_file = {
-          enabled = true,
-          leave_dirs_open = true,
-        },
-        filtered_items = {
-          hide_gitignored = false,
-        },
-      },
-      window = {
-        mappings = {
-          ["<space>"] = "none",
-          ["y"] = "yank_path",
-        },
-      },
-      commands = {
-        yank_path = function(state)
-          -- copy path of current node to unnamed register
-          vim.fn.setreg("+", state.tree:get_node().path)
-        end,
-      },
-    },
-    deactivate = function()
-      vim.cmd([[Neotree close]])
-    end,
-    init = function()
-      vim.g.neo_tree_remove_legacy_commands = 1
-      if vim.fn.argc() == 1 then
-        local stat = vim.loop.fs_stat(vim.fn.argv(0))
-        if stat and stat.type == "directory" then
-          require("neo-tree")
-        end
-      end
-    end,
   },
 }
