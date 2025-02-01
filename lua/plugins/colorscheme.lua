51a9727ff8fa3ad14e2a82eeaf6fc0864c46e762
Fabian-programmer <haefab@gmail.com>
2025-02-01 15:00:44 +0100

    add cattpuchin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,5 +8,5 @@ return {
       require("tokyonight").load()
     end,
   },
-  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000, lazy = false },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000, lazy = false },
 }
