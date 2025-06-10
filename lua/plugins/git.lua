f9d93d571a175c73340976b3ef7d5414da2e2687
Haeussler Fabian (XC-AS/EDE4) <Fabian.Haeussler@bosch.com>
2025-06-10 08:31:58 +0200

    Add overseer fast access

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -6,14 +6,14 @@ return {
       current_line_blame_opts = { delay = 100 },
     },
     -- stylua: ignore
-      keys = {
-        { "<leader>gW", function() require("gitsigns").blame_line{full=true} end, desc = "Git blame line" },
-        { "<leader>gw", function() require("gitsigns").blame() end, desc = "Git blame" },
-        { "gp", function() require("gitsigns").preview_hunk() end, desc = "Git hunk preview" },
-        { "gq", function() require("gitsigns").reset_hunk() end,   desc = "Git hunk clear" },
-        { "<leader>gd", function() require("gitsigns").toggle_deleted() end,   desc = "Git toggle deleted" },
-        { "gü", function() require("gitsigns").nav_hunk('next') end, desc = "Jump to next git hunk" },
-        { "g+", function() require("gitsigns").nav_hunk('prev') end, desc = "Jump to prev git hunk" },
+    keys = {
+      { "<leader>gW", function() require("gitsigns").blame_line { full = true } end, desc = "Git blame line" },
+      { "<leader>gw", function() require("gitsigns").blame() end,                    desc = "Git blame" },
+      { "gp",         function() require("gitsigns").preview_hunk() end,             desc = "Git hunk preview" },
+      { "gq",         function() require("gitsigns").reset_hunk() end,               desc = "Git hunk clear" },
+      { "<leader>gd", function() require("gitsigns").toggle_deleted() end,           desc = "Git toggle deleted" },
+      { "gü",         function() require("gitsigns").nav_hunk('next') end,           desc = "Jump to next git hunk" },
+      { "g+",         function() require("gitsigns").nav_hunk('prev') end,           desc = "Jump to prev git hunk" },
     },
   },
 
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -9,20 +9,12 @@ return {
   -- framework to run tasks
   {
     "stevearc/overseer.nvim",
+    keys = {
+      { "<leader>oo", "<cmd>OverseerOpen<cr>", desc = "Open Overseer" },
+    },
     opts = {
-      -- Tasks are disposed 5 minutes after running to free resources.
-      -- If you need to close a task immediatelly:
-      -- press ENTER in the menu you see after compiling on the task you want to close.
       task_list = {
-        direction = "bottom",
         min_height = 25,
-        max_height = 25,
-        default_detail = 1,
-        bindings = {
-          ["q"] = function()
-            vim.cmd("OverseerClose")
-          end,
-        },
       },
     },
   },
