db5993e634f97249d62782123e518cc36ad31f9e
Fabian Haeussler <haefab@gmail.com>
2025-07-16 21:23:13 +0200

    simplify completion

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -5,47 +5,9 @@ return {
     version = "*",
     dependencies = "rafamadriz/friendly-snippets",
     event = "InsertEnter",
-
     opts = {
-      -- disables autocomplete in rename ui input
-      enabled = function()
-        return not vim.tbl_contains({}, vim.bo.filetype)
-            and vim.bo.buftype ~= "nofile"
-            and vim.bo.buftype ~= "prompt"
-            and vim.b.completion ~= false
-      end,
-      completion = {
-        accept = {
-          auto_brackets = {
-            enabled = true,
-          },
-        },
-        menu = {
-          draw = {
-            treesitter = { "lsp" },
-          },
-        },
-        documentation = {
-          auto_show = true,
-          auto_show_delay_ms = 200,
-        },
-        ghost_text = {
-          enabled = true,
-        },
-      },
-
-      sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
-      },
-      cmdline = {
-        enabled = false
-      },
-
-      keymap = {
-        preset = "enter",
-        ["<C-y>"] = { "select_and_accept" },
-      },
+      keymap = { preset = 'enter' },
+      completion = { documentation = { auto_show = true } },
     },
-    opts_extend = { "sources.default" },
   },
 }
