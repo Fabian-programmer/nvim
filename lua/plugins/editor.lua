d3b2b0a14a4dc073f2e1a93503e8d334c00de9f5
Haeussler Fabian (XC-AS/EDE4) <Fabian.Haeussler@bosch.com>
2025-07-17 07:45:02 +0200

    add trouble integration into snacks

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -138,6 +138,26 @@ return {
   {
     "folke/trouble.nvim",
     cmd = { "TroubleToggle", "Trouble" },
+    specs = {
+      "folke/snacks.nvim",
+      opts = function(_, opts)
+        return vim.tbl_deep_extend("force", opts or {}, {
+          picker = {
+            actions = require("trouble.sources.snacks").actions,
+            win = {
+              input = {
+                keys = {
+                  ["<c-t>"] = {
+                    "trouble_open",
+                    mode = { "n", "i" },
+                  },
+                },
+              },
+            },
+          },
+        })
+      end,
+    },
     opts = { use_diagnostic_signs = true },
     keys = {
       { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>",              desc = "Diagnostics (Trouble)" },
