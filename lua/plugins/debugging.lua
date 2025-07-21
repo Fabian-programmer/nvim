690c9fea70723a642f05fedf6a99cad2cce08c3a
Fabian Haeussler <haefab@gmail.com>
2025-07-21 19:48:55 +0200

    clean up debugging.lua even more

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -34,45 +34,29 @@ return {
     },
 
     keys = {
-      { "<F5>",       function() require("dap").continue() end,                                             desc = "Continue" },
-      { "<F7>",       function() require("dap").step_into() end,                                            desc = "Step Into" },
-      { "<F8>",       function() require("dap").step_over() end,                                            desc = "Step Over" },
-      { "<F9>",       function() require("dap").step_out() end,                                             desc = "Step Out" },
-      { "<F10>",      function() require("dap").run_to_cursor() end,                                        desc = "Run to Cursor" },
-      { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = "Breakpoint Condition" },
-      { "<leader>db", function() require("dap").toggle_breakpoint() end,                                    desc = "Toggle Breakpoint" },
-      { "<leader>dj", function() require("dap").down() end,                                                 desc = "Down" },
-      { "<leader>dk", function() require("dap").up() end,                                                   desc = "Up" },
-      { "<leader>dw", function() require("dap.ui.widgets").hover() end,                                     desc = "Widgets" },
-      { "<leader>dt", function() require("dap").terminate() end,                                            desc = "Terminate" },
-      {
-        "<leader>ds",
-        function()
-          local widgets = require("dap.ui.widgets")
-          widgets.centered_float(widgets.scopes)
-        end,
-        desc = "Scope",
-      },
+      { "<F5>",       function() require("dap").continue() end,                                                  desc = "Continue" },
+      { "<F7>",       function() require("dap").step_into() end,                                                 desc = "Step Into" },
+      { "<F8>",       function() require("dap").step_over() end,                                                 desc = "Step Over" },
+      { "<F9>",       function() require("dap").step_out() end,                                                  desc = "Step Out" },
+      { "<F10>",      function() require("dap").run_to_cursor() end,                                             desc = "Run to Cursor" },
+      { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end,      desc = "Breakpoint Condition" },
+      { "<leader>db", function() require("dap").toggle_breakpoint() end,                                         desc = "Toggle Breakpoint" },
+      { "<leader>dj", function() require("dap").down() end,                                                      desc = "Down" },
+      { "<leader>dk", function() require("dap").up() end,                                                        desc = "Up" },
+      { "<leader>dw", function() require("dap.ui.widgets").hover() end,                                          desc = "Widgets" },
+      { "<leader>dt", function() require("dap").terminate() end,                                                 desc = "Terminate" },
+      ---@format disable-next
+      { "<leader>ds", function() require("dap.ui.widgets").centered_float(require("dap.ui.widgets").scopes) end, desc = "Scope" },
     },
     config = function()
       local dap, dapui = require("dap"), require("dapui")
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
+      dap.listeners.before.attach.dapui_config = dapui.open
+      dap.listeners.before.launch.dapui_config = dapui.open
+      dap.listeners.before.event_terminated.dapui_config = dapui.close
+      dap.listeners.before.event_exited.dapui_config = dapui.close
 
-      vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
-
-      vim.fn.sign_define("DapStopped",
-        { text = " ", texthl = "DiagnosticWarn", linehl = "DapStoppedLine", numhl = "DapStoppedLine" })
+      ---@format disable-next
+      vim.fn.sign_define("DapStopped", { text = " ", texthl = "DiagnosticWarn", linehl = "DapStoppedLine", numhl = "DapStoppedLine" })
       vim.fn.sign_define("DapBreakpoint", { text = " ", texthl = "DiagnosticError" })
       vim.fn.sign_define("DapBreakpointCondition", { text = " ", texthl = "DiagnosticError" })
       vim.fn.sign_define("DapBreakpointRejected", { text = " ", texthl = "DiagnosticError" })
