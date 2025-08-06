6607ae212ad5ff2d02c746c092e6662282da6b53
Haeussler Fabian (XC-AS/EDE4) <Fabian.Haeussler@bosch.com>
2025-08-06 09:21:19 +0200

    exrc: remove nvim-config-local use :trust instead

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -44,19 +44,4 @@ return {
     version = "*",
   },
 
-  {
-    "klen/nvim-config-local",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require('config-local').setup {
-        config_files = { ".nvim.lua", ".nvimrc", ".exrc" },
-        hashfile = vim.fn.stdpath("data") .. "/config-local",
-        autocommands_create = true,
-        commands_create = true,
-        silent = false,
-        lookup_parents = false,
-      }
-    end
-  },
-
 }
